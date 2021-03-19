f4bb625faedc3ec01e1013ae48e4f65f5eaefc21
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-19 14:17:24 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
-import unittest
-import main
+import unittest,main
+
 
 
 class QuizTestCase(unittest.TestCase):
     def test_question_1(self):
         """Question 1."""
         msg = """True and False are boolean values.
-Interestingly, they inherit from ints. Read more here:
-https://medium.com/rmotr-com/those-tricky-python-booleans-2100d5df92c#.hiwfwzcrg
-"""
+        Interestingly, they inherit from ints. Read more here:
+        https://medium.com/rmotr-com/those-tricky-python-booleans-2100d5df92c#.hiwfwzcrg
+        """
         self.assertEqual(main.question_1(), "Boolean", msg)
 
     def test_question_2(self):
